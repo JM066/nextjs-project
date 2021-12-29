554d358ab4d41876f0ccc1d1f4213a07e93e55ed
Jungmin Lee <mina.lee066@gmail.com>
2021-12-29 17:54:06 +0900

    getStaticProps added and in progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import Layout from "../components/Layout";
+import Home from "./index";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }, props) {
+  // console.log("pageProps", props);
   return (
     <Layout>
+      <Home />
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,30 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+function Home({ products }) {
+  console.log("products", products);
+
   return (
     <div className={styles.container}>
+      <ul>
+        {products?.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
       <h1>Hello Next World</h1>
     </div>
   );
 }
+export function getStaticProps() {
+  return {
+    props: {
+      products: [
+        {
+          id: "p1",
+          title: "product 1",
+        },
+      ],
+    },
+  };
+}
+export default Home;
